f0096beaf07d39409bf1e461e75c41130f35332e
rinaldo <rinaldobenaccetta@hotmail.com>
2020-03-07 21:35:56 +0100

    dec: documenting autoloader

--- src/class/Autoloader.php
+++ src/class/Autoloader.php
@@ -1,13 +1,19 @@
 <?php
 
-
+/**
+ * Class Autoloader
+ *
+ * from https://www.grafikart.fr/tutoriels/autoload-561
+ *
+ */
 class Autoloader {
 
   /**
-   * save the autoloader
+   * save the autoloader.
    */
   static function register(){
     spl_autoload_register(array(__CLASS__, 'autoload'));
+    // 'autoload' is for calling autoload method
   }
 
   /**

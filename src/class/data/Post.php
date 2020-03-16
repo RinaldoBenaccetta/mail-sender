4b672eff8f41d8cf8d515c19cfad322d7c04cc00
rinaldo <rinaldobenaccetta@hotmail.com>
2020-03-16 18:38:08 +0100

    feature: prepare sanitize

---
+++ src/class/data/Post.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace MailSender\data;
+
+
+class Post {
+
+  // transform all post entries in string
+  // exept arrays >> transform all entries in strings, exept arrays
+
+  public function __construct($post) {
+
+  }
+
+  public function sanitize() {
+
+  }
+}

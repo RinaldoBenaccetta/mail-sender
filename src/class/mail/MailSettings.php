318dfa39c94b7bbdaac6cf06bd2594da9c83b620
rinaldo <rinaldobenaccetta@hotmail.com>
2020-03-16 16:44:31 +0100

    feature: fix, tests and docs for mail settings

--- src/class/mail/MailSettings.php
+++ src/class/mail/MailSettings.php
@@ -3,33 +3,47 @@
 
 namespace MailSender\mail;
 
-use MailSender\mail\MailOptions;
 use MailSender\render\Render;
-use MailSender\tools\Tools;
+use MailSender\settings\GetSettings;
 
+/**
+ * Class MailSettings
+ *
+ * Filter and complete the values provided by $POST.
+ *
+ *
+ * @package MailSender\mail
+ */
 class MailSettings {
 
-  // global
-  const DEBUG = 'server'; // off | client | server
+  /**
+   * Containing post values provided.
+   *
+   * @var object
+   */
+  private object $_post;
 
-  // DB
-  const HOST = 'smtp.gmail.com';
-  const PORT = 587;
-  const ENCRYPTION = 'STARTTLS'; // STARTTLS or SMTPS
-  const SMTP_AUTHENTICATION = TRUE;
-  const MAIL_LOGIN = 'benaccettarinaldo@gmail.com';
+  /**
+   * Containing the options provided by MailOptions class.
+   *
+   * @var object
+   */
+  private object $_options;
 
-  // sensible
-  const MAIL_PASSWORD = 'vivelaneigeDejuin';
+  /**
+   * The settings imported from Settings class.
+   *
+   * @var object
+   */
+  private object $_settings;
 
-  private $post;
-  private $options;
 
   public function __construct($post) {
     // todo : validate post
 
-    $this->post = $this->getPost($post);
-    $this->options = $this->getOptions($this->post);
+    $this->_settings = GetSettings::getSettings();
+    $this->_post     = $this->getPost($post);
+    $this->_options  = $this->getOptions($this->_post);
   }
 
   private function getOptions($options) {
@@ -41,92 +55,217 @@ class MailSettings {
     return (object) $post;
   }
 
+  /**
+   * Return host from settings/GetSettings.
+   *
+   * @return mixed
+   */
   public function getHost() {
-    return self::HOST;
+    return $this->_settings->mailServer->host;
   }
 
+  /**
+   * Return port from settings/GetSettings.
+   *
+   * @return mixed
+   */
   public function getPort() {
-    return self::PORT;
+    return $this->_settings->mailServer->port;
   }
 
+  /**
+   * Return encryption from settings/GetSettings.
+   *
+   * @return mixed
+   */
   public function getEncryption() {
-    return self::ENCRYPTION;
+    return $this->_settings->mailServer->encryption;
   }
 
+  /**
+   * Return smtp authentication from settings/GetSettings.
+   *
+   * @return mixed
+   */
   public function getSmtpAuthentication() {
-    return self::SMTP_AUTHENTICATION;
+    return $this->_settings->mailServer->smtpAuthentication;
   }
 
+  /**
+   * Return mail login from settings/GetSettings.
+   *
+   * @return mixed
+   */
   public function getMailLogin() {
-    return self::MAIL_LOGIN;
+    return $this->_settings->mailServer->mailLogin;
   }
 
+  /**
+   * Return mail password from settings/GetSettings.
+   *
+   * @return mixed
+   */
+  // todo : improve security on this.
   public function getMailPassword() {
-    return self::MAIL_PASSWORD;
+    return $this->_settings->mailServer->mailPassword;
   }
 
+  /**
+   * Return sender mail from POST
+   * OR
+   * Return default sender mail from settings/GetSettings
+   * According to MailOptions class.
+   *
+   * @return string|null
+   */
   public function getSenderMail() {
-    if (isset($this->options->senderMail) && !is_null($this->options->senderMail)) {
-      return $this->options->senderMail;
+    if (!empty($this->_options->senderMail)) {
+      return $this->_options->senderMail;
     }
     return NULL;
   }
 
+  /**
+   * Return sender name from POST.
+   * OR
+   * Return default sender name from settings/GetSettings
+   * According to MailOptions class.
+   *
+   * @return string|null
+   */
   public function getSenderName() {
-    if (isset($this->options->senderName) && !is_null($this->options->senderName)) {
-      return $this->options->senderName;
+    if (!empty($this->_options->senderName)) {
+      return $this->_options->senderName;
     }
     return NULL;
   }
 
+  /**
+   * Return reply mail from POST.
+   * OR
+   * Return default sender mail from settings/GetSettings
+   * According to MailOptions class.
+   *
+   * @return string|null
+   */
   public function getReplyMail() {
-    if (isset($this->post->replyMail) && !is_null($this->post->replyMail)) {
-      return $this->post->replyMail;
+    if (!empty($this->_post->replyMail)) {
+      return $this->_post->replyMail;
     }
     return NULL;
   }
 
+  /**
+   * Return reply name from POST.
+   * OR
+   * Return default sender name from settings/GetSettings
+   * According to MailOptions class.
+   *
+   * @return string|null
+   */
   public function getReplyName() {
-    if (isset($this->post->replyName) && !is_null($this->post->replyName)) {
-      return $this->post->replyName;
+    if (!empty($this->_post->replyName)) {
+      return $this->_post->replyName;
     }
     return NULL;
   }
 
+  /**
+   * Return recipient mail from POST.
+   * OR
+   * Return default recipient mail from settings/GetSettings
+   * According to MailOptions class.
+   *
+   * @return string|null
+   */
   public function getRecipientMail() {
-    if (isset($this->options->recipientMail) && !is_null($this->options->recipientMail)) {
-      return $this->options->recipientMail;
+    if (!empty($this->_options->recipientMail)) {
+      return $this->_options->recipientMail;
     }
     return NULL;
   }
 
+  /**
+   * Return recipient name from POST.
+   * OR
+   * Return default recipient name from settings/GetSettings
+   * According to MailOptions class.
+   *
+   * @return string|null
+   */
   public function getRecipientName() {
-    if (isset($this->options->recipientName) && !is_null($this->options->recipientName)) {
-      return $this->options->recipientName;
+    if (!empty($this->_options->recipientName)) {
+      return $this->_options->recipientName;
     }
     return NULL;
   }
 
+  /**
+   * Return debug for PHPMailer
+   * in terms of environment from settings/GetSettings.
+   * 'dev' will return 'server'.
+   * other will return 'off'.
+   *
+   * @return string
+   */
   public function getDebug() {
-    return self::DEBUG;
+    $environment = NULL;
+    if (!empty($this->_settings->global->environment)) {
+      $environment = $this->_settings->global->environment;
+    }
+    switch ($environment) {
+      case 'dev':
+        return 'server';
+        break;
+
+      case 'prod':
+        return 'off';
+        break;
+
+      default:
+        return 'off';
+    }
   }
 
+  /**
+   * Return subject from POST
+   * OR
+   * Return subject according to setSubject from MailOptions class.
+   *
+   * @return string|null
+   */
   public function getSubject() {
-    if (isset($this->options->subject) && !is_null($this->options->subject)) {
-      return $this->options->subject;
+    if (!empty($this->_options->subject)) {
+      return $this->_options->subject;
     }
     return NULL;
   }
 
+  /**
+   * Return the HTML body with the variables provided in post
+   * and with the template provided in post.
+   * If no template is provided in POST,
+   * the default template will be used.
+   *
+   * @return string
+   * @throws \Twig\Error\LoaderError
+   * @throws \Twig\Error\RuntimeError
+   * @throws \Twig\Error\SyntaxError
+   */
   public function getHtmlBody() {
-    $template = $this->options->template;
-    $data = (array) $this->post;
+    $template = $this->_options->template;
+    $data = (array) $this->_post;
     return Render::render($template, $data);
   }
 
+  /**
+   * Return the alternative body provided in POST.
+   *
+   * @return string|null
+   */
   public function getAltBody() {
-    if (property_exists($this->post, 'altbody')) {
-      return $this->post->altbody;
+    if (property_exists($this->_post, 'altBody')) {
+      return $this->_post->altBody;
     } else {
       return NULL;
     }
--- tests/MailSettingsTest.php
+++ tests/MailSettingsTest.php
@@ -1,39 +1,207 @@
 <?php
 
+require __DIR__ . "/../src/class/mail/DefaultContact.php";
+require __DIR__ . "/../src/class/tools/StringTool.php";
+require __DIR__ . "/../src/class/render/Render.php";
 require __DIR__ . "/../src/class/mail/MailSettings.php";
 require __DIR__ . "/../src/class/settings/Settings.php";
 require __DIR__ . "/../src/class/settings/GetSettings.php";
-require __DIR__ . "/../src/class/mail/DefaultContact.php";
 require __DIR__ . "/../src/class/mail/MailOptions.php";
 require __DIR__ . "/../src/class/tools/Tools.php";
 
-
+use MailSender\mail\DefaultContact;
 use MailSender\mail\MailSettings;
-//use MailSender\settings\GetSettings;
-//use MailSender\settings\Settings;
+use MailSender\settings\GetSettings;
 
 class MailSettingsTest extends PHPUnit\Framework\TestCase {
 
-//  public function invokeMethod(&$object, $methodName, array $parameters = array())
-//  {
-//    $reflection = new \ReflectionClass(get_class($object));
-//    $method = $reflection->getMethod($methodName);
-//    $method->setAccessible(true);
-//    return $method->invokeArgs($object, $parameters);
-//  }
-//
-//  public function testGetPost() {
-//    $myClass = new MailSettings([]);
-//    $object = $this->invokeMethod($myClass, 'getPost', []);
-//    //$this->assertEquals('WI', $state);
-//
-//    //$mailSettings = new MailSettings();
-//    $this->assertIsObject($object);
-//  }
-
   public function testGetHost() {
+    $settings = GetSettings::getSettings();
     $mailSettings = new MailSettings([]);
-    $this->assertEquals('test', $mailSettings->getHost());
+    $this->assertEquals($settings->mailServer->host, $mailSettings->getHost());
   }
+
+  public function testGetPort() {
+    $settings = GetSettings::getSettings();
+    $mailSettings = new MailSettings([]);
+    $this->assertEquals($settings->mailServer->port, $mailSettings->getPort());
+  }
+
+  public function testGetEncryption() {
+    $settings = GetSettings::getSettings();
+    $mailSettings = new MailSettings([]);
+    $this->assertEquals($settings->mailServer->encryption, $mailSettings->getEncryption());
+  }
+
+  public function testGetSmtpAuthentication() {
+    $settings = GetSettings::getSettings();
+    $mailSettings = new MailSettings([]);
+    $this->assertEquals($settings->mailServer->smtpAuthentication, $mailSettings->getSmtpAuthentication());
+  }
+
+  public function testGetMailLogin() {
+    $settings = GetSettings::getSettings();
+    $mailSettings = new MailSettings([]);
+    $this->assertEquals($settings->mailServer->mailLogin, $mailSettings->getMailLogin());
+  }
+
+  public function testGetMailPassword() {
+    $settings = GetSettings::getSettings();
+    $mailSettings = new MailSettings([]);
+    $this->assertEquals($settings->mailServer->mailPassword, $mailSettings->getMailPassword());
+  }
+
+  public function testGetSenderMailWithMailProvided() {
+    $senderMail = 't800@skynet.com';
+    $mailSettings = new MailSettings([
+      'senderMail'=>$senderMail
+    ]);
+    $this->assertEquals($senderMail, $mailSettings->getSenderMail());
+  }
+
+  public function testGetSenderMailWithoutMailProvided() {
+    $settings = GetSettings::getSettings();
+    $mailSettings = new MailSettings([]);
+    $this->assertEquals($settings->defaultMailOptions->senderMail, $mailSettings->getSenderMail());
+  }
+
+  public function testGetSenderNameWithNameProvided() {
+    $senderName = 'Sarah Connor';
+    $mailSettings = new MailSettings([
+      'senderName'=>$senderName
+    ]);
+    $this->assertEquals($senderName, $mailSettings->getSenderName());
+  }
+
+  public function testGetSenderNameWithoutNameProvided() {
+    $settings = GetSettings::getSettings();
+    $mailSettings = new MailSettings([]);
+    $this->assertEquals($settings->defaultMailOptions->senderName, $mailSettings->getSenderName());
+  }
+
+  public function testGetReplyMailWithNameProvided() {
+    $replyMail = 't800@skynet.com';
+    $mailSettings = new MailSettings([
+      'replyMail'=>$replyMail
+    ]);
+    $this->assertEquals($replyMail, $mailSettings->getReplyMail());
+  }
+
+  public function testGetReplyMailWithoutNameProvided() {
+    $mailSettings = new MailSettings([]);
+    $this->assertEquals(NULL, $mailSettings->getReplyMail());
+  }
+
+  public function testGetReplyNameWithNameProvided() {
+    $replyName = 'Sarah Connor';
+    $mailSettings = new MailSettings([
+      'replyName'=>$replyName
+    ]);
+    $this->assertEquals($replyName, $mailSettings->getReplyName());
+  }
+
+  public function testGetReplyNameWithoutNameProvided() {
+    $mailSettings = new MailSettings([]);
+    $this->assertEquals(NULL, $mailSettings->getReplyName());
+  }
+
+  public function testGetRecipientMailWithMailProvided() {
+    $recipientMail = 't800@skynet.com';
+    $mailSettings = new MailSettings([
+      'recipientMail'=>$recipientMail
+    ]);
+    $this->assertEquals($recipientMail, $mailSettings->getRecipientMail());
+  }
+
+  public function testGetRecipientMailWithoutMailProvided() {
+    $settings = GetSettings::getSettings();
+    $mailSettings = new MailSettings([]);
+    $this->assertEquals($settings->defaultMailOptions->recipientMail, $mailSettings->getRecipientMail());
+  }
+
+  public function testGetRecipientNameWithNameProvided() {
+    $recipientName = 'Sarah Connor';
+    $mailSettings = new MailSettings([
+      'recipientName'=>$recipientName
+    ]);
+    $this->assertEquals($recipientName, $mailSettings->getRecipientName());
+  }
+
+  public function testGetRecipientNameWithoutNameProvided() {
+    $settings = GetSettings::getSettings();
+    $mailSettings = new MailSettings([]);
+    $this->assertEquals($settings->defaultMailOptions->recipientName, $mailSettings->getRecipientName());
+  }
+
+  public function testGetSubjectWithSubjectProvidedWithDefaultTemplate() {
+    $settings = GetSettings::getSettings();
+    $subject = "I'll be back";
+    $expected = DefaultContact::getSubject((object) [
+      'senderName' => $settings->defaultMailOptions->senderName
+    ]);
+    $mailSettings = new MailSettings([
+      'subject' => $subject
+    ]);
+    $this->assertEquals($expected, $mailSettings->getSubject());
+  }
+
+  public function testGetSubjectWithoutSubjectProvidedWithDefaultTemplate() {
+    $settings = GetSettings::getSettings();
+    $expected = DefaultContact::getSubject((object) [
+      'senderName' => $settings->defaultMailOptions->senderName
+    ]);
+    $mailSettings = new MailSettings([]);
+    $this->assertEquals($expected, $mailSettings->getSubject());
+  }
+
+  public function testGetSubjectWithSubjectProvidedAndNotDefaultTemplate() {
+    $subject = "I'll be back";
+    $mailSettings = new MailSettings([
+      'template' => 'test-template',
+      'subject' => $subject
+    ]);
+    $this->assertEquals($subject, $mailSettings->getSubject());
+  }
+
+  public function testGetSubjectWithoutSubjectProvidedAndNotDefaultTemplate() {
+    $settings = GetSettings::getSettings();
+    $mailSettings = new MailSettings([
+      'template' => 'test-template',
+    ]);
+    $this->assertEquals($settings->defaultMailOptions->subject, $mailSettings->getSubject());
+  }
+
+  public function testGetHtmlBody() {
+    $message = 'Hello';
+    $name = 'Sarah Connor';
+    $expected = "Hello <strong>Sarah Connor</strong>";
+
+    $mailSettings = new MailSettings([
+      'message' => $message,
+      'name' => $name,
+      'template' => 'test-template',
+    ]);
+
+    $this->assertEquals($expected, $mailSettings->getHtmlBody());
+  }
+
+  public function testGetAltBodyWithAltBodyProvided() {
+    $expected = "Hello Sarah Connor";
+
+    $mailSettings = new MailSettings([
+      'altBody' => $expected,
+    ]);
+
+    $this->assertEquals($expected, $mailSettings->getAltBody());
+  }
+
+  public function testGetAltBodyWithNoAltBodyProvided() {
+    $mailSettings = new MailSettings([]);
+
+    $this->assertEquals(NULL, $mailSettings->getAltBody());
+  }
+
+
+
 
 }

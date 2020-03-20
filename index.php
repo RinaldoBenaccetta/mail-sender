6155d8a902b016bd8c7fe85ffadcf3d486c462d8
rinaldo <rinaldobenaccetta@hotmail.com>
2020-03-20 20:07:28 +0100

    clean: clean index file

--- index.php
+++ index.php
@@ -1,36 +1,22 @@
 <?php
 
 use MailSender\data\Post;
-use MailSender\mail\MailSettings;
-use MailSender\mail\MailSend;
-use MailSender\settings\Settings;
-use MailSender\settings\GetSettings;
-use MailSender\tools\Debug;
-use MailSender\Path;
 use MailSender\data\Server;
+use MailSender\mail\MailSend;
+use MailSender\mail\MailSettings;
 
-//use MailSender\render\Render;
-
-//require '../vendor/autoload.php';
 require './vendor/autoload.php';
-//require './src/class/Autoloader.php';
-//
-//// Call the autoloader
-//MailSender\Autoloader::register();
 
-$test = new Server();
-//var_dump($test->getServerSettings());
-dump($test->getServerSettings());
+//$test = new Server();
+//dump($test->getServerSettings());
 
 
-//$post = new Post();
-//$mailSettings = new MailSettings($post->getPost($_POST));
-//
-//
-//$options = $mailSettings->getAll();
+$post = new Post();
+$mailSettings = new MailSettings($post->getPost($_POST));
+$options = $mailSettings->getAll();
 
 // send the mail
-//new MailSend($options);
+new MailSend($options);
 
 
 

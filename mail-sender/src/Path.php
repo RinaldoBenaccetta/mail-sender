aa654778fc824c1d2798fb8c21203cf631ed77e6
rinaldo <rinaldobenaccetta@hotmail.com>
2020-04-04 12:41:08 +0200

    fix: fix path that don't work with distant server

--- mail-sender/src/Path.php
+++ mail-sender/src/Path.php
@@ -8,11 +8,11 @@ class Path
 {
 
     const ROOT_PATH = __DIR__;
-    const TEMPLATES = self::ROOT_PATH . './templates';
-    const CLASS_ROOT = self::ROOT_PATH . './class';
-    const MAIL = self::CLASS_ROOT . './mail';
-    const RENDER = self::CLASS_ROOT . './render';
-    const SETTINGS = self::CLASS_ROOT . './settings';
-    const TOOLS = self::CLASS_ROOT . './tools';
+    const TEMPLATES = self::ROOT_PATH . '/./templates';
+    const CLASS_ROOT = self::ROOT_PATH . '/./class';
+    const MAIL = self::CLASS_ROOT . '/./mail';
+    const RENDER = self::CLASS_ROOT . '/./render';
+    const SETTINGS = self::CLASS_ROOT . '/./settings';
+    const TOOLS = self::CLASS_ROOT . '/./tools';
     const PARENT_ROOT = '../' . self::ROOT_PATH;
 }
